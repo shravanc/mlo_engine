f548fb3a96371d642dcb60d3af34e8d0f9f22e7e
“shravanc” <shravan007.c@gmail.com>
2018-06-12 14:09:29 +0530

    subdomain support added

--- mlo_engine/app/controllers/mlo_engine/searches_controller.rb
+++ mlo_engine/app/controllers/mlo_engine/searches_controller.rb
@@ -3,9 +3,9 @@ require_dependency "mlo_engine/application_controller"
 module MloEngine
   class SearchesController < ApplicationController
 		def index
-			subdomain = request.subdomain
-			subdomain = subdomain.gsub('-tdbank')
-      
+			subdomain = request.subdomains(0).first
+			subdomain = subdomain.gsub('-tdbank', '').to_s
+
 		  user = User.find_by( :subdomain => subdomain ).as_json(:only => [ :first_name, :last_name, :email, :job_title, :bio ] )				
 
       render json: { mlo: user }
